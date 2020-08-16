db019d38dac30a2857265da1aaac878002d31e70
Arya Talathi <55396585+aryatalathi@users.noreply.github.com>
2020-08-16 10:23:16 +0530

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,21 @@
+L = []
+
+N = int(raw_input())
+
+for i in range(0, N):
+    t = raw_input().split()
+
+    if t[0] == 'insert':
+        L.insert(int(t[1]), int(t[2]))
+    elif t[0] == 'print':
+        print L
+    elif t[0] == 'remove':
+        L.remove(int(t[1]))
+    elif t[0] == 'append':
+        L.append(int(t[1]))
+    elif t[0] == 'sort':
+        L.sort()
+    elif t[0] == 'pop':
+        L.pop()
+    elif t[0] == 'reverse':
+        L.reverse()
